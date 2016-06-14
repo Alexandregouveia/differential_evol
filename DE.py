76f2c18459e11811ce88e2fcfb3eae61db008a6c
Alexandre <alexandre.vsgouveia@gmail.com>
2016-06-14 10:50:05 -0300

    faltam mais testes adicionar k-means

--- DE.py
+++ DE.py
@@ -5,7 +5,7 @@ from random import randint as rint
 def DE(gen,f):
 
     base = csv.reader(open('base.txt'), delimiter= ' ')
-    entrada,pais, ngen =[], [], 1
+    entrada,pais, ngen = [], [], 0
     for i in base:
         aux = []
         for j in i:
@@ -13,21 +13,25 @@ def DE(gen,f):
         entrada.append(aux)
     pais = entrada
     cent1, cent2 = distancia(pais)
-
-    while ngen<gen:# condicao de parada
+    for i in range(len(pais)):
+        fit = fitness(pais[i], pais[cent1], pais[cent2])
+        pais[i].append(fit)
+    while ngen<gen:  # condicao de parada
+        filhos = []
         for i in range(len(pais)):
-            if (i==len(pais)-1): # para o penultimo elementos
-                filho = crossover(pais[i], pais[i + 1])
-            elif (i==len(pais)): # utiliza o primeiro elemento na multacao do ultimo
+            if i == len(pais)-1:  # para o ultimo elementos
                 filho = crossover(pais[i], pais[0])
+            elif i == len(pais)-2:  #para o penultimo
+                filho = crossover(pais[i], pais[i+1])
             else:
-                if rint(0,1)==0:# Escolhe aleatoriamente entre crossover ou mutacao
+                if rint(0, 1) == 0:  # Escolhe aleatoriamente entre crossover ou mutacao
                     filho = mutacao(pais[i], pais[i+1], pais[i+2], f)
                 else:
                     filho = crossover(pais[i], pais[i+1])
-            ngen +=1
-
-
+            filho.append(fitness(filho, pais[cent1], pais[cent2]))
+            filhos.append(filho)
+        print(filhos)
+        ngen += 1
 
 
 def crossover(pai1, pai2):
@@ -37,7 +41,8 @@ def crossover(pai1, pai2):
             filho.append(pai1[i])
         else:
             filho.append(pai2[i])
-    return (filho)
+    return filho
+
 
 def mutacao(pai, vizinho1, vizinho2, f):
     filho = []
@@ -45,6 +50,7 @@ def mutacao(pai, vizinho1, vizinho2, f):
         filho.append(pai[i]+f*(vizinho1[i]-vizinho2[i]))
 
     return filho
+
 
 def selecao(pais, filhos):
     saida = []
@@ -54,6 +60,7 @@ def selecao(pais, filhos):
         else:
             saida.append(filhos[i])
     return saida
+
 
 def distancia(matriz):# escolhe aleatoriamente os centroides
     while True:
@@ -66,10 +73,11 @@ def distancia(matriz):# escolhe aleatoriamente os centroides
             break
     return nun, nun2
 
-def fitness(matriz, cent1, cent2):
-    for i in range(len(matriz)):
-        if matriz[i][2]==1:
-            j= (matriz[i][0]-matriz[cent1][0])**2+ (matriz[i][1]-matriz[cent1][1])**2
 
+def fitness(matriz, cent1, cent2):# informar as instancias nao os indices
+    j=((matriz[0]-cent1[0])**2 + (matriz[1]-cent1[1])**2) + ((matriz[0]-cent2[0])**2 + (matriz[1]-cent2[1])**2)
+    fit = 2*(j +0.1)
+    return fit
 
 '''Work in progress'''
+DE(1,2)
