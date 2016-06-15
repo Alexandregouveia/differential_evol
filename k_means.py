95ce7d7bb2089eb8dc05c956af4adbb4bd4cedd9
Alexandre <alexandre.vsgouveia@gmail.com>
2016-06-15 11:08:12 -0300

    faltam mais testes finalizar k-means

--- k_means.py
+++ k_means.py
@@ -14,6 +14,16 @@ def k_means():
     cent1, cent2 = distancia(pais)
     centroid1 = pais[cent1]
     centroid2 = pais[cent2]
+    cluster1 = []
+    cluster2 = []
+    for i in range(len(pais)):
+        if calc_dist(pais[i], centroid1) > calc_dist(pais[i], centroid2):  # classe 1
+            pais[2] = 1
+            centroid1 = att_dist(pais[i], centroid1, cluster1)
+        else:  # classe -1
+            pais[2] = -1
+            centroid1 = att_dist(pais[i], centroid2)
+
     for i in range(10): # 10 iteracoes
         for i in range(len(pais)):
             if calc_dist(pais[i], centroid1) > calc_dist(pais[i], centroid2):# classe 1
@@ -29,8 +39,15 @@ def calc_dist(pai, cent1):
     return dist
 
 
-def att_dist(pai, cent):
+def att_dist(pai, cent, cluster):
     novo = []
-    novo.append((pai[0] + cent[0])/2)
-    novo.append((pai[1] + cent[1]) / 2)
+    for i in range(len(cluster)):
+        cent[0] = pai[0] + cent[0]
+        cent[1] = pai[1] + cent[1]
+    novo.append(cent[0]/len(cluster))
+    novo.append(cent[1] / len(cluster))
+
     return novo
+
+def att_dist2(pai, cent):
+    cent[0] = pai[0]
